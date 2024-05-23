762a11b3e97c3892b728e35208e679bd62f09fba
annebanannekaffeekanne <annika.haschko@stud.hshl.de>
2024-05-23 13:17:50 +0200

    added a few documents

--- objektorientierte_programmierung/ep_exercise2.py
+++ objektorientierte_programmierung/ep_exercise2.py
@@ -1,1 +1,19 @@
-# neues Verzeichnis
+#import pip
+#import pylint
+import pytest
+
+def vowels(text: str) -> int:
+    count = 0
+    vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+    for i in text:
+        if i in vowels:
+            count += 1
+    return count
+
+
+text = "Hallo"
+print("Anzahl der Vokale:", vowels(text))
+
+if __name__ == '__ep_exercise2__':
+    pytest.ep_exercise2()
--- objektorientierte_programmierung/projecteuler.py
+++ objektorientierte_programmierung/projecteuler.py
@@ -1,1 +1,91 @@
-# neues Verzeichnis
+from math import sqrt
+import pytest
+
+
+# multiples of 3 or 5
+# Find the sum of all the multiples of 3 or 5 below 1000.
+def problem1(n):
+    list = []  # empty list
+    sum = 0  # sets variable sum = 0
+    for i in range(0, n - 1):
+        list.append(i)  # creates list with numbers from 0 to 999
+        if list[i] // 3 or list[i] // 5:  # checks if listindex is a multiple of 3 or 5
+            sum += list[i]  # if it is, the listindex is added to the sum
+    print(sum)
+
+
+problem1(1000)
+
+
+# even ficonacci numbers
+# By considering the terms in the Fibonacci sequence whose values
+# do not exceed four million, find the sum of the even-valued terms.
+def problem2(n):
+    a = 1
+    b = 2
+    list = []  # empty list
+    sum = 0
+    if n < 4000000:  # upper border 4 mio
+        for i in range(n):
+            c = a + b  # fibonacci-algorithm
+            a = b
+            b = c
+            if b % 2 == 0:  # selects the even numbers
+                list.append(b)  # adds them to the list
+                sum += b  # adds value to the sum
+        print(list)
+        print(sum)
+
+
+problem2(10)
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    for i in range(2, int(sqrt(n)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+# largest prime factor
+# What is the largest prime factor of n?
+def problem3(n):
+    primes = []  # empty list
+    for i in range(2, n):
+        if is_prime(i):  # checks if it is a prime number
+            primes.append(i)  # if it is it adds the number to the list
+    # print(primes)
+    list = []  # new empty list
+    for j in range(len(primes)):  # runs through the prime numbers
+        if n % primes[j] == 0:  # checks if the number is divisible by one of the primes
+            list.append(primes[j])  # if it is, it is added to the list
+    print(max(list))  # returns the highest number of the list = largest prime factor
+
+
+problem3(13546)
+
+
+# largest palindrome product
+# Find the largest palindrome made from the product of two 3-digit numbers.
+def is_palindromic(n):
+    return str(n) == str(n)[::-1]
+
+
+def problem4(m):  # m=upper border
+    max_palindrome = 0  # set variable = 0
+    for i in range(m):  # first number to the last
+        for j in range(m):
+            product = i * j
+            if is_palindromic(product) and is_palindromic(product) > max_palindrome:
+                max_palindrome = product
+    print(max_palindrome)
+
+
+problem4(1000)  # 3-digit, upper border
+
+if __name__ == '__projecteuler__':
+    pytest.projecteuler()
